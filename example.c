5bcf98960cde4bc887ad27e73ca19a2cf84a74ae
Menderes Sabaz <sabazmenderes@gmail.com>
2025-10-26 16:11:45 +0300

    License

--- example.c
+++ example.c
@@ -12,25 +12,25 @@ typedef struct {
     char* name;
 } config_t;
 
+config_t conf = {
+    .no_color = false,
+    .off_count = 0,
+    .scale = 1.0,
+    .size = 0,
+    .name = NULL,
+};
+bool help = false;
+flag_t flags[] = {
+    { .flag = "nocolor", .desc = "Prints output without color", .addr = &conf.no_color, .type = as_bool },
+    { .flag = "off",     .desc = "Sets the off count (integer)", .addr = &conf.off_count, .type = as_i64 },
+    { .flag = "scale",   .desc = "Scaling factor (double)", .addr = &conf.scale, .type = as_double },
+    { .flag = "size",    .desc = "Buffer size with suffix (e.g. 10K, 2M)", .addr = &conf.size, .type = as_size },
+    { .flag = "name",    .desc = "Sets a name (string)", .addr = &conf.name, .type = as_str },
+    { .flag = "help",    .desc = "prints the help text", .addr = &help, .type = as_bool },
+};
+
 int main(int argc, char** argv)
 {
-    config_t conf = {
-        .no_color = false,
-        .off_count = 0,
-        .scale = 1.0,
-        .size = 0,
-        .name = NULL,
-    };
-    bool help = false;
-    flag_t flags[] = {
-        { .flag = "nocolor", .desc = "Prints output without color", .addr = &conf.no_color, .type = as_bool },
-        { .flag = "off",     .desc = "Sets the off count (integer)", .addr = &conf.off_count, .type = as_i64 },
-        { .flag = "scale",   .desc = "Scaling factor (double)", .addr = &conf.scale, .type = as_double },
-        { .flag = "size",    .desc = "Buffer size with suffix (e.g. 10K, 2M)", .addr = &conf.size, .type = as_size },
-        { .flag = "name",    .desc = "Sets a name (string)", .addr = &conf.name, .type = as_str },
-        { .flag = "help",    .desc = "prints the help text", .addr = &help, .type = as_bool },
-    };
-
     if (!flags_parse(flags, sizeof(flags)/sizeof(flag_t), &argc, &argv)) {
         flags_error();
         fprintf(stderr, "Use --help for usage info.\n");
--- flags.h
+++ flags.h
@@ -384,3 +384,21 @@ bool flag_size_calculate_multiplier(char* endptr,uint64_t* result)
  }
 
 #endif // FLAGS_IMPLEMENTATION
+
+/*
+ *  flags - simple command-line flag parser in C
+ *  Copyright (C) 2025 Menderes Sabaz <sabazmenders@proton.me>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
